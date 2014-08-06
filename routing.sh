1a75ad8d42f0041a7537d5fcaac9135234944ccb
Chun-Yeow Yeoh <yeohchunyeow@gmail.com>
2014-08-06 11:40:16 +0800

    set the multicast routing rule last
    Bringing up the AP and only then set the multicast
    routing rule.
    
    Signed-off-by: Chun-Yeow Yeoh <yeohchunyeow@gmail.com>

--- routing.sh
+++ routing.sh
@@ -8,6 +8,6 @@ iptables -F
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 iptables -A FORWARD -i wlan1 -o eth0 -j ACCEPT
 echo '1' > /proc/sys/net/ipv4/ip_forward
-route add 239.255.255.250 dev wlan1
 service dnsmasq restart
 hostapd /etc/hostapd/hostapd.conf &
+route add 239.255.255.250 dev wlan1
